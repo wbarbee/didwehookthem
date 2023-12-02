0c39079e0b6df5ec06c3e8eacd8bf40711b548cd
Will Barbee <will.clayton.barbee@gmail.com>
2023-12-02 16:05:00 -0600

    update

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -1,6 +1,6 @@
 const Loading = () => {
 	return (
-		<div className='-mt-[5rem] md:-mt-[1rem] mb-2rem md:mb-0 flex flex-col justify-center align-center w-full h-screen animate-fade-in'>
+		<div className='-mt-[2.5rem] md:-mt-[2rem] mb-2rem md:mb-0 flex flex-col justify-center align-center w-full h-screen animate-fade-in'>
 			<h1 className='text-[6cqw] md:text-[4cqw] lg:text-[3cqw] text-burntOrange text-center'>
 				Did we hook them?
 			</h1>
