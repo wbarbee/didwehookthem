20940a4cef3b1a95fd430c676b7b03f209466b1d
Will Barbee <will.clayton.barbee@gmail.com>
2023-12-01 23:57:03 -0600

    update leading

--- app/page.tsx
+++ app/page.tsx
@@ -70,7 +70,7 @@ const Home = () => {
 			)}
 			{data.gameStatus !== 'Final' && (
 				<div className='-mt-[2rem] md:mt-0 mb-2rem md:mb-0 max-w-[55rem] w-[95%] mx-auto text-center pt-5 pb-8 bg-gray-200 dark:bg-gray-900 rounded-lg shadow-sm relative animate-fade-in'>
-					<h1 className='text-[6.5cqw] md:text-[3rem] lg:text-[3.25rem] text-burntOrange font-graduate leading-[2.2rem] md:leading-[3.2rem]'>
+					<h1 className='text-[6.5cqw] md:text-[3rem] lg:text-[3.25rem] text-burntOrange font-graduate leading-[2.2rem] md:leading-[3.4rem]'>
 						{data.gameStatus === 'Scheduled' ? (
 							<>
 								Game Day: <br />
