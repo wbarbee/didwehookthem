fee788eeaaaad4ffff034d5098f9105877d398d8
Will Barbee <will.clayton.barbee@gmail.com>
2023-12-01 23:56:09 -0600

    update

--- app/page.tsx
+++ app/page.tsx
@@ -70,10 +70,15 @@ const Home = () => {
 			)}
 			{data.gameStatus !== 'Final' && (
 				<div className='-mt-[2rem] md:mt-0 mb-2rem md:mb-0 max-w-[55rem] w-[95%] mx-auto text-center pt-5 pb-8 bg-gray-200 dark:bg-gray-900 rounded-lg shadow-sm relative animate-fade-in'>
-					<h1 className='text-[6.5cqw] md:text-[3rem] lg:text-[3.25rem] text-burntOrange font-graduate'>
-						{data.gameStatus === 'Scheduled'
-							? 'Game Day. Will we hook them?'
-							: 'Currently trying to hook them.'}
+					<h1 className='text-[6.5cqw] md:text-[3rem] lg:text-[3.25rem] text-burntOrange font-graduate leading-[2.2rem] md:leading-[3.2rem]'>
+						{data.gameStatus === 'Scheduled' ? (
+							<>
+								Game Day: <br />
+								Will we hook them?
+							</>
+						) : (
+							'Currently trying to hook them.'
+						)}
 					</h1>
 					<div className='mt-[1rem] mb-[2rem] md:mt-[2rem] md:mb-[2.5rem] w-[95%] max-w-[15rem] mx-auto animate-pulse-opacity'>
 						<img src={hornsSrc} alt='Texas Longhorns Logo' />
