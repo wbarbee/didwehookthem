3a70756e890e936cb1aa3e067bedaaef848ff1a9
Will Barbee <will.clayton.barbee@gmail.com>
2023-12-11 15:23:21 -0600

    add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Footer from './components/Footer';
 import Nav from './nav';
-import { Analytics } from '@vercel/analytics/react';
-import './globals.css';
 import NextEventSlider from './components/NextEventSlider';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import './globals.css';
 
 const bodyClasses =
 	'dark:bg-gray-950 bg-white font-graduate w-full h-full min-h-screen ';
@@ -19,6 +20,7 @@ export default function RootLayout({
 				<NextEventSlider />
 				<Nav />
 				<Analytics />
+				<SpeedInsights />
 				<div className='min-h-screen'>{children}</div>
 				<Footer />
 			</body>
